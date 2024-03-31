4219bdd7f07ec2b1727ed458812256e6a442c17d
Sunny Gupta <you@example.com>
2024-03-31 22:21:32 +0530

    Quiz commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,49 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "What is the chemical symbol for Gold?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?",
+            "What is the tallest mountain in the world?",
+            "Which planet is known as the Red Planet?",
+            "Which one is not the programming language?",
+            "How many teeth does an adult human have",
+            "What type of animal is a penguin?",
+            "What is the fastest land animal?"
+
+
+
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Gd","Go","Ag","Au"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"},
+            {" K2","Mount Everest","Mount Kilimanjaro"," Denali"},
+            {"Venus","Jupiter","Mars","Saturn "},
+            {"Java","Kotlin","Notepad","Python"},
+            {"32","30","28","34"},
+            {"Mammal","Reptile","Insect","Bird"},
+            {"Ostrich","Cheetah","Lion","Elephant"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Au",
+            "Youtube",
+            "Apple",
+            "Mount Everest",
+            "Mars",
+            "Notepad",
+            "32",
+            "Bird",
+            "Cheetah"
+
+    };
+
+}
